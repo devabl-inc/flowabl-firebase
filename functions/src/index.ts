1748484ea0f5a9b0d2f5b32d849d56eed3bd247a
Tim Bula <timrbula@gmail.com>
2021-10-04 19:13:19 -0500

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,12 @@
 import * as functions from "firebase-functions";
 import axios from "axios";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+// Start writing Firebase Functions
 
+// See auth trigger documentation https://firebase.google.com/docs/functions/auth-events
 exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
-  // [END onCreateTrigger]
-  // [START eventAttributes]
-  const email = user.email; // The email of the user.
-  const displayName = user.displayName; // The display name of the user.
-  // [END eventAttributes]
-
+  const email = user.email;
+  const displayName = user.displayName;
   return sendWelcomeEmail(email, displayName);
 });
 
@@ -19,6 +14,8 @@ exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
 async function sendWelcomeEmail(email: string | undefined, displayName: string | undefined) {
   const FLOW_WEBOOK_URL = "https://app.flowabl.io/services/listener/webhook";
   const JOIN_EMAIL_WORKFLOW_ID = "61454e2d1000b141daa8f85f";
+
+  // See env config https://firebase.google.com/docs/functions/config-env
   const FLOW_ACCESS_TOKEN = functions.config().welcome_email.flow_access_token;
 
   try {
